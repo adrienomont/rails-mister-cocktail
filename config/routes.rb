174f28932283efc6d5721f1807e6cf10aa25b82d
adrienomont <adriomont@gmail.com>
2017-08-11 11:21:20 +0200

    Add figaro - Protect my secret data in config/application.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails do
-    resources :doses
+  resources :cocktails, only: [:show, :index, :new, :create] do
+    resources :doses, only: [:create]
   end
   root 'cocktails#index'
+  resources(:doses, { only: [:destroy]})
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-# require 'open-uri'
-# require 'json'
+require 'open-uri'
+require 'json'
 
 # def random_ingredient
-#   url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-#   attempt_serialized = open(url).read
-#   attempt_result = JSON.parse(attempt_serialized)
-#   return attempt_result["drinks.strIngredient1"]
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+attempt_serialized = open(url).read
+attempt_result = JSON.parse(attempt_serialized)
+Ingredient.create(name: attempt_result["strIngredient1"])
 # end
 
 # Ingredient.create(name: random_ingredient.first)
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
 
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Caïpi")
-Cocktail.create(name: "Piña Colada")
+# Cocktail.create(name: "Mojito")
+# Cocktail.create(name: "Caïpi")
+# Cocktail.create(name: "Piña Colada")
 
